9edb9a2a7b9608c042db44ae2a48ef24eca5e290
mariaathoang <mahoang@uia.no>
2023-04-21 18:03:38 +0000

    kommentar

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/mariaathoang/is105sem03
+module github.com/mariaathoang/sem03simpleclient
 
 go 1.18
+
+require github.com/mariaathoang/is105sem03 v0.0.0-20230421063415-c2e3458b8e9b
--- tcp_client.go
+++ tcp_client.go
@@ -18,6 +18,8 @@ func main() {
  	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
 	log.Println("Kryptert melding: ", string(kryptertMelding))
 	_, err = conn.Write([]byte(string(kryptertMelding)))
+	dekryptertMelding := mycrypt.Krypter([]rune(string(kryptertMelding)), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+	log.Println("Dekryptert melding: ", string(dekryptertMelding))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,7 +27,10 @@ func main() {
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
+	}
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
+
+	dekryptertSvar := string(mycrypt.Krypter([]rune(response), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4))
+	log.Printf("Decrypted reply from proxy: %s", dekryptertSvar)
 }
